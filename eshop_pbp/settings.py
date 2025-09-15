73faf4fa27e5556cda65a1009e488e80f322d7a9
chazelnut <rashikamaharani@gmail.com>
2025-09-15 13:43:54 +0700

    CSRF_TRUSTED_ORIGINS FIXED

--- eshop_pbp/settings.py
+++ eshop_pbp/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "rashika-maharani-eshoppbp.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "<https://rashika-maharani-eshoppbp.pbp.cs.ui.ac.id>"
+    "https://rashika-maharani-eshoppbp.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
